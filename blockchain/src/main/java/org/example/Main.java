082e9e2cc8ef94f9c95b7f61140702905af67a66
Sahil <sahil.sonyo@gmail.com>
2023-07-22 16:07:06 +0530

    updated log system

--- blockchain/src/main/java/org/example/Main.java
+++ blockchain/src/main/java/org/example/Main.java
@@ -3,19 +3,24 @@ package org.example;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 // This annotation instructs Spring to initialize its configuration - which is needed to start a
 // new application
 @SpringBootApplication
+@RestController
 @ComponentScan(basePackages = {"org.example.Service","org.example.Controller"})
 public class Main {
 
-    // We can start our application by calling the run method with the primary class
     public static void main(String[] args) {
         SpringApplication.run(Main.class, args);
     }
 
+    @PostMapping("shut-down")
+    public void shutdown() {
+        SpringApplication.exit(SpringApplication.run(Main.class));
+
+    }
 
 }
--- blockchainClient/src/main/java/org/example/Controller/InitiateUserService.java
+++ blockchainClient/src/main/java/org/example/Controller/InitiateUserService.java
@@ -6,15 +6,20 @@ import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
 import org.apache.http.util.EntityUtils;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-import javax.sound.midi.Soundbank;
 import java.io.IOException;
 @Service
+@RestController
 public class InitiateUserService {
 
+    private static final Logger logger = LogManager.getLogger(InitiateUserService.class);
     private String url;
-    private void callUserRegistration(String action) throws Exception{
+    private void callUserRegistration(String action) throws IOException{
 
         CloseableHttpClient httpClient = HttpClients.createDefault();
         url = "http://localhost:8080/" + action;
@@ -25,7 +30,7 @@ public class InitiateUserService {
             HttpResponse response = httpClient.execute(httpGet);
             HttpEntity entity = response.getEntity();
             String responseMessage = EntityUtils.toString(entity);
-            System.out.println(responseMessage);
+            logger.info(responseMessage);
         }catch(IOException e){
             e.printStackTrace();
         }finally {
@@ -35,9 +40,18 @@ public class InitiateUserService {
                 e.printStackTrace();
             }
         }
-
     }
+    @RequestMapping("new-user")
     public void newUserRegistration() throws Exception{
         callUserRegistration("new-user");
+    }
+    public void intiateServiceServer(){
+        ExecuteJarController executeJarController = new ExecuteJarController();
+        String response = executeJarController.executeJar();
+        logger.info(response);
+    }
+
+    public void shutDownServer() throws Exception{
+        callUserRegistration("shut-down");
     }
 }
